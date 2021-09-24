d5ea831d83938f326aef665cae1bb9a1940f3a31
Cristian Vargas <PC-83084@DESKTOP-PFML2T4>
2021-09-23 19:14:13 -0500

    Cargue algoritmo util más no funcional

--- Voraz/Program.cs
+++ Voraz/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Voraz
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] deno = { 5, 10, 20, 25 };
+
+        static int n = deno.Length;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int n = 40;
+            Console.Write("La menor cantidad de monedas para el valor " + n + " es: ");
+
+            BuscarCambio(n);
         }
+
+        public static void BuscarCambio(int V)
+        {
+            List<int> resp = new List<int>();
+
+            // Traverse through all denomination
+            for (int i = n - 1; i >= 0; i--)
+            {
+                // Find denominations
+                while (V >= deno[i])
+                {
+                    V -= deno[i];
+                    resp.Add(deno[i]);
+                }
+                
+            }
+
+            // Print result
+            for (int i = 0; i < resp.Count; i++)
+            {
+                Console.Write(" " + resp[i]);
+            }
+
+        }
+
     }
 }
