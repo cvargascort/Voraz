29acb05f8d32dfaa21257ec987bd5ae21527ae52
Cristian Vargas <PC-83084@DESKTOP-PFML2T4>
2021-09-23 19:57:52 -0500

    Cargue algoritmo voraz funcional

--- Voraz/Program.cs
+++ Voraz/Program.cs
@@ -5,36 +5,60 @@ namespace Voraz
 {
     class Program
     {
-        static int[] deno = { 5, 10, 20, 25 };
+        static int[] deno = { 20, 10, 5, 2, 1 };
 
         static int n = deno.Length;
 
         static void Main()
         {
-            int n = 40;
+            int n = 36;
             Console.Write("La menor cantidad de monedas para el valor " + n + " es: ");
 
             BuscarCambio(n);
         }
 
-        public static void BuscarCambio(int V)
+        public static void BuscarCambio(int valor)
         {
-            List<int> resp = new List<int>();
-
-            // Traverse through all denomination
-            for (int i = n - 1; i >= 0; i--)
+            int valorTemp = valor;
+            List<int> resp = new List<int>();            
+            int indiceInicio = n - 1;
+            int contador = 0;
+            int i = 0;
+            while (contador != 3)
             {
-                // Find denominations
-                while (V >= deno[i])
-                {
-                    V -= deno[i];
-                    resp.Add(deno[i]);
+                List<int> respTemp = new List<int>();
+                valor = valorTemp;
+                for (i = contador; i <= indiceInicio; i++)
+                {                    
+                    // Buscar denominacion
+                    while (valor >= deno[i])
+                    {
+                        valor -= deno[i];
+                        respTemp.Add(deno[i]);
+                    }
                 }
+
+                indiceInicio = indiceInicio - 1;
+                contador = contador + 1;
+
+                if (resp.Count == 0)
+                {
+                    resp = respTemp;
+                }
+                else if (resp.Count > respTemp.Count && respTemp.Count != 0)
+                {
+                    resp = respTemp;
+                }
+                /*
+                for (int respT = 0; respT < respTemp.Count; respT++)
+                {
+                    respTemp.RemoveAt(respT);
+                }*/
                 
             }
-
+            
             // Print result
-            for (int i = 0; i < resp.Count; i++)
+            for (i = 0; i < resp.Count; i++)
             {
                 Console.Write(" " + resp[i]);
             }
