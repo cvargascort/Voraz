f3012b543e1410353c98d50f7f73267386f8451e
Cristian Vargas <PC-83084@DESKTOP-PFML2T4>
2021-09-23 21:38:20 -0500

    Algoritmo terminado y funcional

--- Voraz/Program.cs
+++ Voraz/Program.cs
@@ -5,65 +5,157 @@ namespace Voraz
 {
     class Program
     {
-        static int[] deno = { 20, 10, 5, 2, 1 };
+        static int[] deno = { 1, 2, 5, 10, 20 };
 
         static int n = deno.Length;
 
         static void Main()
         {
+            //EjecucionAlgoritmo1();
+            EjecucionAlgoritmo3();
+        }
+
+        static void EjecucionAlgoritmo1()
+        {
             int n = 36;
             Console.Write("La menor cantidad de monedas para el valor " + n + " es: ");
 
-            BuscarCambio(n);
+            BuscarCambioMoneda(n);
         }
 
-        public static void BuscarCambio(int valor)
+        static void EjecucionAlgoritmo3()
         {
-            int valorTemp = valor;
-            List<int> resp = new List<int>();            
-            int indiceInicio = n - 1;
-            int contador = 0;
-            int i = 0;
-            while (contador != 3)
+            string texto = "ABAABCB";
+            Console.Write("El texto lexograficamente mayor para " + texto + " es: ");
+
+            BuscarLetras(texto.ToUpper());
+        }
+
+        public static void BuscarCambioMoneda(int V)
+        {
+            List<int> resp = new List<int>();
+
+            // Traverse through all denomination
+            for (int i = n - 1; i >= 0; i--)
             {
-                List<int> respTemp = new List<int>();
-                valor = valorTemp;
-                for (i = contador; i <= indiceInicio; i++)
-                {                    
-                    // Buscar denominacion
-                    while (valor >= deno[i])
-                    {
-                        valor -= deno[i];
-                        respTemp.Add(deno[i]);
-                    }
-                }
-
-                indiceInicio = indiceInicio - 1;
-                contador = contador + 1;
-
-                if (resp.Count == 0)
+                // Find denominations
+                while (V >= deno[i])
                 {
-                    resp = respTemp;
-                }
-                else if (resp.Count > respTemp.Count && respTemp.Count != 0)
-                {
-                    resp = respTemp;
-                }
-                /*
-                for (int respT = 0; respT < respTemp.Count; respT++)
-                {
-                    respTemp.RemoveAt(respT);
-                }*/
-                
+                    V -= deno[i];
+                    resp.Add(deno[i]);
+                }                
             }
-            
+
             // Print result
-            for (i = 0; i < resp.Count; i++)
+            for (int i = 0; i < resp.Count; i++)
             {
                 Console.Write(" " + resp[i]);
             }
-
         }
 
+        public static void BuscarLetras(string texto)
+        {
+            List<string> resp = new List<string>();
+            List<int> textoNum = new List<int>();
+            List<int> textNumOrdenada = new List<int>();
+            
+            textoNum = ConvertirToNumero(texto);
+
+            while (textoNum.Count != textNumOrdenada.Count)
+            {
+                for (int i = 0; i < textoNum.Count; i++)
+                {
+                    if (i != 0)
+                    {
+                        var menorLista = textNumOrdenada[0];
+
+                        if (textoNum[i] >= menorLista)
+                        {
+                            for (int j = textNumOrdenada.Count - 1; j  != -1; j--)
+                            {
+                                //j = 0
+                                if (j == textNumOrdenada.Count - 1)
+                                {
+                                    textNumOrdenada.Add(textNumOrdenada[j]);
+                                }
+                                else
+                                {
+                                    textNumOrdenada[j + 1] = textNumOrdenada[j];
+                                }                                
+                            }
+                            textNumOrdenada[0] = textoNum[i];
+                        }
+                        else
+                        {
+                            textNumOrdenada.Add(textoNum[i]);
+                        }
+                    }
+                    else
+                    {
+                        textNumOrdenada.Add(textoNum[i]);
+                    }
+                }
+            }
+
+            resp = ConvertirToLetra(textNumOrdenada);
+
+            // Print result
+            for (int i = 0; i < resp.Count; i++)
+            {
+                Console.Write(resp[i]);
+            }
+        }
+
+        public static List<int> ConvertirToNumero(string texto)
+        {
+            List<int> resp = new List<int>();
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (Char.IsLetter(Convert.ToChar(texto[i])))
+                {
+                    switch (texto[i])
+                    {
+                        case 'A':
+                            resp.Add(1);
+                            break;
+                        case 'B':
+                            resp.Add(2);
+                            break;
+                        case 'C':
+                            resp.Add(3);
+                            break;
+                        case 'D':
+                            resp.Add(4);
+                            break;
+                    }
+                }
+            }
+            return resp;
+        }
+
+        public static List<string> ConvertirToLetra(List<int> num)
+        {
+            List<string> resp = new List<string>();
+
+            for (int i = 0; i < num.Count; i++)
+            {
+                switch (num[i])
+                {
+                    case 1:
+                        resp.Add("A");
+                        break;
+                    case 2:
+                        resp.Add("B");
+                        break;
+                    case 3:
+                        resp.Add("C");
+                        break;
+                    case 4:
+                        resp.Add("D");
+                        break;
+                }
+            }
+            return resp;
+        }
     }
 }
